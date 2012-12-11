852f1c6b7d7dece586b44b548d1a7b2e81e358c9
Jasdeep Jaitla <jasdeep@scalabl3.com>
2012-12-11 13:38:52 -0800

    first commit

---
+++ TwitterHipster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterHipster::Application.config.secret_token = 'f6ffe9ec1d06e4b9e35a1611927de0076c0124b74f5a2f46a863b8e335db758ba867261e04b1791158ff71499287f4b4456279ed66ca29ce06cece49eddcaf6e'
